4d4c3564ab98102509156aba3011f155347f9d5e
Dasser Basyouni <daser4ever96@gmail.com>
2018-09-11 18:21:01 +0200

    chore: Updated libraries and Gradle
    - Updated runner version 1.0.1 > 1.0.2.
    - Updated rules version 1.0.1 > 1.0.2.
    - Updated espresson-core version 3.0.1 > 3.0.2.
    - Updated butterknife version 8.5.1 > 8.8.1.
    - Updated butterknife-compiler version 8.5.1 > 8.8.1.
    - Updated kotlin version 1.2.21 > 1.2.51.
    - Updated Gradle Wrapper distribution gradle-4.4.1-bin.zip > gradle-4.4.1-all.zip.
    - Updated Gradle classpath 3.0.1 > 3.1.4.
    - Updated picasso version 2.5.2 > 2.71828.
    
    - Refractor Picasso.with(context) to Picasso.get().
    - Refractor onBitmapFailed(Drawable) to onBitmapFailed(Exception, Drawable).

--- belvedere/src/main/java/zendesk/belvedere/FixedWidthImageView.java
+++ belvedere/src/main/java/zendesk/belvedere/FixedWidthImageView.java
@@ -185,7 +185,7 @@ public class FixedWidthImageView extends AppCompatImageView implements Target {
     }
 
     @Override
-    public void onBitmapFailed(Drawable errorDrawable) {
+    public void onBitmapFailed(Exception e, Drawable errorDrawable) {
         // intentionally empty
     }
 
